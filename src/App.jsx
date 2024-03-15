fb8d53483328ca650806d1984b312a97e20886d8
JOVI-DESKTOP\Rondo <ronnie@govbuilt.com>
2024-03-15 13:42:06 -0700

    refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { dracula } from "react-syntax-highlighter/dist/esm/styles/prism";
+import KeyList from "./KeyList";
+
 import "./App.css";
 
 function App() {
@@ -80,27 +82,7 @@ function App() {
             />
           </div>
         </div>
-        <div className="rounded-md border-blue-500 border-2 p-2 mx-12 transform-gpu transition-all duration-200 ease-in-out">
-          <h2 className="text-blue-500 font-bold border-b-2 border-blue-400">
-            Api Keys
-          </h2>
-          {inputs.map((input, index) => (
-            <div
-              key={index}
-              className="flex justify-between m-2 border-[1px] rounded-md border-slate-400"
-            >
-              <p className="text-slate-50 p-2">{input}</p>
-              <button
-                onClick={() => {
-                  setInputs(inputs.filter((_, i) => i !== index));
-                }}
-                className="rounded-md hover:shadow-lg hover:shadow-blue-500 transition-all duration-200 ease-in-out"
-              >
-                x
-              </button>
-            </div>
-          ))}
-        </div>
+        <KeyList inputs={inputs} setInputs={setInputs} />
       </div>
     </>
   );
---
+++ src/KeyList.jsx
@@ -0,0 +1,27 @@
+function KeyList(inputs, setInputs) {
+  return (
+    <div className="rounded-md border-blue-500 border-2 p-2 mx-12 transform-gpu transition-all duration-200 ease-in-out">
+      <h2 className="text-blue-500 font-bold border-b-2 border-blue-400">
+        Api Keys
+      </h2>
+      {inputs.map((input, index) => (
+        <div
+          key={index}
+          className="flex justify-between m-2 border-[1px] rounded-md border-slate-400"
+        >
+          <p className="text-slate-50 p-2">{input}</p>
+          <button
+            onClick={() => {
+              setInputs(inputs.filter((_, i) => i !== index));
+            }}
+            className="rounded-md hover:shadow-lg hover:shadow-blue-500 transition-all duration-200 ease-in-out"
+          >
+            x
+          </button>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default KeyList;
