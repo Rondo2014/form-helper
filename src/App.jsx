1f570f206fd2c1a96a60393d2f6a2f20e88a4e1e
JOVI-DESKTOP\Rondo <ronnie@govbuilt.com>
2024-03-15 14:18:31 -0700

    Refactoring and Styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 import { useEffect, useState } from "react";
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { dracula } from "react-syntax-highlighter/dist/esm/styles/prism";
 import Keys from "./Keys";
 
 import "./App.css";
+import TotalCost from "./TotalCost";
 
 function App() {
   const [output, setOutput] = useState("");
@@ -28,61 +27,23 @@ function App() {
     setOutput(totalCost(inputs));
   }, [inputs]);
 
-  function handleSubmit() {
-    const prefixedInputValue = "data." + inputValue;
-    setInputs([...inputs, prefixedInputValue]);
-    setInputValue("");
-  }
-
-  function copyToClipboard() {
-    navigator.clipboard.writeText(output);
-  }
-
   useEffect(() => {
     console.log(inputs);
   }, [inputs]);
 
   return (
     <>
-      <div className="flex flex-row">
-        <div className="flex flex-col">
-          <div className="mb-4 mx-12 w-full">
-            <input
-              id="textBox"
-              type="text"
-              placeholder="Input Api Key"
-              className="rounded-md p-2 hover:shadow-lg hover:shadow-blue-500 transition-all duration-200 ease-in-out hover:-translate-y-1 border-2 border-blue-500"
-              value={inputValue}
-              onChange={(e) => setInputValue(e.target.value)}
-              onKeyDown={(e) => (e.key === "Enter" ? handleSubmit() : null)}
-            />{" "}
-            <button
-              onClick={handleSubmit}
-              className="rounded-md hover:shadow-lg hover:shadow-blue-500 transition-all duration-200 ease-in-out hover:-translate-y-1"
-            >
-              Submit
-            </button>
-          </div>
-          <div className="col-span-2 rounded-md p-2 mx-12 transform-gpu transition-all duration-200 ease-in-out">
-            <div className="rounded-lg shadow-md shadow-slate-600">
-              <SyntaxHighlighter language="javascript" style={dracula}>
-                {output}
-              </SyntaxHighlighter>
-            </div>
-            <button
-              onClick={copyToClipboard}
-              className="rounded-md hover:shadow-lg hover:shadow-blue-500 transition-all duration-200 ease-in-out hover:-translate-y-[2px] self-center mt-4"
-            >
-              Copy
-            </button>
-            <textarea
-              value={output}
-              onChange={(e) => setOutput(e.target.value)}
-              className="w-full h-48 rounded-lg p-2 border-2 border-gray-300 resize-none mt-4"
-            />
-          </div>
-        </div>
-        <Keys inputs={inputs} setInputs={setInputs} />
+      <div className="grid grid-cols-2 align-middle max-w-[1920px] mx-auto max-h-screen mt-48">
+        <TotalCost
+          output={output}
+          setOutput={setOutput}
+          inputValue={inputValue}
+          setInputValue={setInputValue}
+          inputs={inputs}
+          setInputs={setInputs}
+          className="w-2/3"
+        />
+        <Keys inputs={inputs} setInputs={setInputs} className="w-1/3" />
       </div>
     </>
   );
---
+++ src/TotalCost.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { dracula } from "react-syntax-highlighter/dist/esm/styles/prism";
+
+function TotalCost({
+  output,
+  setOutput,
+  inputValue,
+  setInputValue,
+  inputs,
+  setInputs,
+}) {
+  function handleSubmit() {
+    const prefixedInputValue = "data." + inputValue;
+    setInputs([...inputs, prefixedInputValue]);
+    setInputValue("");
+  }
+
+  function copyToClipboard() {
+    navigator.clipboard.writeText(output);
+  }
+  return (
+    <div className="grid grid-cols-1 w-full">
+      <div className="mb-4 mx-12 w-full">
+        <input
+          id="textBox"
+          type="text"
+          placeholder="Input Api Key"
+          className="rounded-md p-2 hover:shadow-lg hover:shadow-blue-500 transition-all duration-200 ease-in-out border-2 border-blue-500 text-blue-950"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          onKeyDown={(e) => (e.key === "Enter" ? handleSubmit() : null)}
+        />{" "}
+        <button onClick={handleSubmit}>Submit</button>
+      </div>
+      <div className="col-span-2 rounded-md p-2 mx-12 transform-gpu transition-all duration-200 ease-in-out">
+        <div className="rounded-lg shadow-md shadow-slate-600">
+          <SyntaxHighlighter language="javascript" style={dracula}>
+            {output}
+          </SyntaxHighlighter>
+        </div>
+        <button onClick={copyToClipboard} className="mt-4">
+          Copy
+        </button>
+        <textarea
+          value={output}
+          onChange={(e) => setOutput(e.target.value)}
+          className="w-full h-48 rounded-lg p-2 border-2 border-gray-300 resize-none mt-4 bg-slate-800 text-white"
+          autoCorrect="false"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default TotalCost;
